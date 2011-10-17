phacility/phabricator

commit f58e63a9178d90029fccc45d42ac80edff9838b4

Install proper git package in Ubuntu install script.
'git-core' provided the 'git' command in Ubuntu LTS.

scripts/install/install_ubuntu.sh
@@ -5,6 +5,8 @@ confirm() {
   read -e ignored
 }
 
+GIT='git'
+
 ISSUE=`cat /etc/issue`
 if [[ $ISSUE != Ubuntu* ]]
 then
@@ -12,6 +14,9 @@ then
   echo "to be something else. Your results may vary.";
   echo
   confirm
+elif [[ $ISSUE != "Ubuntu 10.04*" ]]
+then
+  GIT='git-core'
 fi
 
 echo "PHABRICATOR UBUNTU INSTALL SCRIPT";
@@ -37,7 +42,7 @@ echo
 set +x
 
 sudo apt-get -qq update
-sudo apt-get install git mysql-server apache2 php5 php5-mysql php5-gd php5-dev php5-curl php-apc php5-cli dpkg-dev
+sudo apt-get install $GIT mysql-server apache2 php5 php5-mysql php5-gd php5-dev php5-curl php-apc php5-cli dpkg-dev
 
 HAVEPCNTL=`php -r "echo extension_loaded('pcntl');"`
 if [ $HAVEPCNTL != "1" ]

tomo/facebook-phabricator

commit a0c06e8c5b714e65e520a177f125bc48b4886afe

Fix string matching bug in Ubuntu install script.
Commit f58e63a9178d90029fccc45d42ac80edff9838b4 introduced a bug that
always set GIT to 'git-core'.

scripts/install/install_ubuntu.sh
@@ -7,6 +7,7 @@ confirm() {
 
 GIT='git'
 
+LTS="Ubuntu 10.04"
 ISSUE=`cat /etc/issue`
 if [[ $ISSUE != Ubuntu* ]]
 then
@@ -14,7 +15,7 @@ then
   echo "to be something else. Your results may vary.";
   echo
   confirm
-elif [[ $ISSUE != "Ubuntu 10.04*" ]]
+elif [[ `expr match "$ISSUE" "$LTS"` -eq ${#LTS} ]]
 then
   GIT='git-core'
 fi