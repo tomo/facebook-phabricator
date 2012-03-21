phacility/phabricator

commit 30ae22bfcff71fa3f14e38fd3cd597448df501c3

Fix many encoding and architecture problems in Diffusion request and URI handling
Summary:
Diffusion request/uri handling is currently a big, hastily ported mess. In particular, it has:

  - Tons and tons of duplicated code.
  - Bugs with handling unusual branch and file names.
  - An excessively large (and yet insufficiently expressive) API on DiffusionRequest, including a nonsensical concrete base class.
  - Other tools were doing hacky things like passing ":" branch names.

This diff attempts to fix these issues.

  - Make the base class abstract (it was concrete ONLY for "/diffusion/").
  - Move all URI generation to DiffusionRequest. Make the core static. Add unit tests.
  - Delete the 300 copies of URI generation code throughout Diffusion.
  - Move all URI parsing to DiffusionRequest. Make the core static. Add unit tests.
  - Add an appropriate static initializer for other callers.
  - Convert all code calling `newFromAphrontRequestDictionary` outside of Diffusion to the new `newFromDictionary` API.
  - Refactor static initializers to be sensibly-sized.
  - Refactor derived DiffusionRequest classes to remove duplicated code.
  - Properly encode branch names (fixes branches with "/", see <https://github.com/facebook/phabricator/issues/100>).
  - Properly encode path names (fixes issues in D1742).
  - Properly escape delimiter characters ";" and "$" in path names so files like "$100" are not interpreted as "line 100".
  - Fix a couple warnings.
  - Fix a couple lint issues.
  - Fix a bug where we would not parse filenames with spaces in them correctly in the Git browse query.
  - Fix a bug where Git change queries would fail unnecessarily.
  - Provide or improve some documentation.

This thing is pretty gigantic but also kind of hard to split up. If it's unreasonably difficult to review, let me know and I can take a stab at it though.

This supplants D1742.

Test Plan:

src/applications/owners/query/path/PhabricatorOwnerPathQuery.php
@@ -22,7 +22,12 @@ final class PhabricatorOwnerPathQuery {
     PhabricatorRepository $repository,
     PhabricatorRepositoryCommit $commit) {
 
-    $drequest = self::buildDiffusionRequest($repository, $commit);
+    $drequest = DiffusionRequest::newFromAphrontRequestDictionary(
+      array(
+        'repository'  => $repository,
+        'commit'      => $commit->getCommitIdentifier(),
+      ));
+
     $path_query = DiffusionPathChangeQuery::newFromDiffusionRequest(
       $drequest);
     $paths = $path_query->loadChanges();
@@ -36,17 +41,6 @@ final class PhabricatorOwnerPathQuery {
       $result[] = $basic_path;
     }
     return $result;
-  }
-
-  private static function buildDiffusionRequest(
-    PhabricatorRepository $repository,
-    PhabricatorRepositoryCommit $commit) {
-
-    return DiffusionRequest::newFromAphrontRequestDictionary(
-      array(
-        'callsign'  => $repository->getCallsign(),
-        'commit'    => $commit->getCommitIdentifier(),
-      ));
   }
 
 }

src/__phutil_library_map__.php
@@ -340,6 +340,7 @@ phutil_register_library_map(array(
     'DiffusionSvnRequest' => 'applications/diffusion/request/svn',
     'DiffusionSymbolController' => 'applications/diffusion/controller/symbol',
     'DiffusionSymbolQuery' => 'applications/diffusion/query/symbol',
+    'DiffusionURITestCase' => 'applications/diffusion/request/base/__tests__',
     'DiffusionView' => 'applications/diffusion/view/base',
     'DrydockAllocator' => 'applications/drydock/allocator/resource',
     'DrydockAllocatorWorker' => 'applications/drydock/allocator/worker',
@@ -1188,6 +1189,7 @@ phutil_register_library_map(array(
     'DiffusionSvnLastModifiedQuery' => 'DiffusionLastModifiedQuery',
     'DiffusionSvnRequest' => 'DiffusionRequest',
     'DiffusionSymbolController' => 'DiffusionController',
+    'DiffusionURITestCase' => 'ArcanistPhutilTestCase',
     'DiffusionView' => 'AphrontView',
     'DrydockAllocatorWorker' => 'PhabricatorWorker',
     'DrydockCommandInterface' => 'DrydockInterface',

src/aphront/default/configuration/AphrontDefaultApplicationConfiguration.php
@@ -247,30 +247,13 @@ class AphrontDefaultApplicationConfiguration
         '' => 'DiffusionHomeController',
         '(?P<callsign>[A-Z]+)/' => array(
           '' => 'DiffusionRepositoryController',
-          'repository/'.
-            '(?P<path>[^/]+)/'
-              => 'DiffusionRepositoryController',
-          'change/'.
-            '(?P<path>.*?)'.
-            '(?:[;](?P<commit>[a-z0-9]+))?'
-              => 'DiffusionChangeController',
-          'history/'.
-            '(?P<path>.*?)'.
-            '(?:[;](?P<commit>[a-z0-9]+))?'
-              => 'DiffusionHistoryController',
-          'browse/'.
-            '(?P<path>.*?)'.
-            '(?:[;](?P<commit>[a-z0-9]+))?'.
-            '(?:[$](?P<line>\d+(?:-\d+)?))?'
-              => 'DiffusionBrowseController',
-          'diff/'.
-            '(?P<path>.*?)'.
-            '(?:[;](?P<commit>[a-z0-9]+))?'
-              => 'DiffusionDiffController',
-          'lastmodified/'.
-            '(?P<path>.*?)'.
-            '(?:[;](?P<commit>[a-z0-9]+))?'
-              => 'DiffusionLastModifiedController',
+
+          'repository/(?P<dblob>.*)'    => 'DiffusionRepositoryController',
+          'change/(?P<dblob>.*)'        => 'DiffusionChangeController',
+          'history/(?P<dblob>.*)'       => 'DiffusionHistoryController',
+          'browse/(?P<dblob>.*)'        => 'DiffusionBrowseController',
+          'lastmodified/(?P<dblob>.*)'  => 'DiffusionLastModifiedController',
+          'diff/'                       => 'DiffusionDiffController',
         ),
         'inline/(?P<phid>[^/]+)/' => 'DiffusionInlineCommentController',
         'services/' => array(

src/applications/conduit/method/diffusion/getrecentcommitsbypath/ConduitAPI_diffusion_getrecentcommitsbypath_Method.php
@@ -45,13 +45,13 @@ final class ConduitAPI_diffusion_getrecentcommitsbypath_Method
   }
 
   protected function execute(ConduitAPIRequest $request) {
-    $results = array();
+    $drequest = DiffusionRequest::newFromDictionary(
+      array(
+        'callsign'  => $request->getValue('callsign'),
+        'path'      => $request->getValue('path'),
+      ));
 
-    $history = DiffusionHistoryQuery::newFromDiffusionRequest(
-      DiffusionRequest::newFromAphrontRequestDictionary(
-        $request->getAllParameters()
-      )
-    )
+    $history = DiffusionHistoryQuery::newFromDiffusionRequest($drequest)
     ->setLimit(self::RESULT_LIMIT)
     ->needDirectChanges(true)
     ->needChildChanges(true)

src/applications/diffusion/controller/base/DiffusionController.php
@@ -21,8 +21,10 @@ abstract class DiffusionController extends PhabricatorController {
   protected $diffusionRequest;
 
   public function willProcessRequest(array $data) {
-    $this->diffusionRequest = DiffusionRequest::newFromAphrontRequestDictionary(
-      $data);
+    if (isset($data['callsign'])) {
+      $drequest = DiffusionRequest::newFromAphrontRequestDictionary($data);
+      $this->diffusionRequest = $drequest;
+    }
   }
 
   public function setDiffusionRequest(DiffusionRequest $request) {
@@ -31,6 +33,9 @@ abstract class DiffusionController extends PhabricatorController {
   }
 
   protected function getDiffusionRequest() {
+    if (!$this->diffusionRequest) {
+      throw new Exception("No Diffusion request object!");
+    }
     return $this->diffusionRequest;
   }
 
@@ -73,37 +78,34 @@ abstract class DiffusionController extends PhabricatorController {
     }
 
     $drequest = $this->getDiffusionRequest();
-    $repository = $drequest->getRepository();
-    $callsign = $repository->getCallsign();
 
-    $branch_uri = $drequest->getBranchURIComponent($drequest->getBranch());
-    $path_uri = $branch_uri.$drequest->getPath();
+    foreach ($navs as $action => $name) {
+      $href = $drequest->generateURI(
+        array(
+          'action' => $action,
+        ));
 
-    $commit_uri = null;
-    $raw_commit = $drequest->getRawCommit();
-    if ($raw_commit) {
-      $commit_uri = ';'.$drequest->getCommitURIComponent($raw_commit);
-    }
-
-    foreach ($navs as $uri => $name) {
       $nav->addNavItem(
         phutil_render_tag(
           'a',
           array(
-            'href'  => "/diffusion/{$callsign}/{$uri}/{$path_uri}{$commit_uri}",
+            'href'  => $href,
             'class' =>
-              ($uri == $selected
+              ($action == $selected
                 ? 'aphront-side-nav-selected'
                 : null),
           ),
           $name));
     }
+
+    // TODO: URI encoding might need to be sorted out for this link.
+
     $nav->addNavItem(
       phutil_render_tag(
         'a',
         array(
           'href'  => '/owners/view/search/'.
-            '?repository='.phutil_escape_uri($callsign).
+            '?repository='.phutil_escape_uri($drequest->getCallsign()).
             '&path='.phutil_escape_uri('/'.$drequest->getPath()),
         ),
         'Search Owners'));
@@ -158,11 +160,18 @@ abstract class DiffusionController extends PhabricatorController {
   }
 
   private function buildCrumbList(array $spec = array()) {
-    $drequest = $this->getDiffusionRequest();
 
     $crumb_list = array();
 
-    $repository = $drequest->getRepository();
+    // On the home page, we don't have a DiffusionRequest.
+    if ($this->diffusionRequest) {
+      $drequest = $this->getDiffusionRequest();
+      $repository = $drequest->getRepository();
+    } else {
+      $drequest = null;
+      $repository = null;
+    }
+
     if ($repository) {
       $crumb_list[] = phutil_render_tag(
         'a',
@@ -231,14 +240,9 @@ abstract class DiffusionController extends PhabricatorController {
         return $crumb_list;
     }
 
-    $branch_uri = $drequest->getBranchURIComponent($drequest->getBranch());
-    $view_root_uri = "/diffusion/{$callsign}/{$view}/{$branch_uri}";
-    $jump_href = $view_root_uri;
-
-    $view_tail_uri = null;
-    if ($raw_commit) {
-      $view_tail_uri = ';'.$drequest->getCommitURIComponent($raw_commit);
-    }
+    $uri_params = array(
+      'action' => $view,
+    );
 
     if (!strlen($path)) {
       $crumb_list[] = $view_name;
@@ -247,7 +251,10 @@ abstract class DiffusionController extends PhabricatorController {
       $crumb_list[] = phutil_render_tag(
         'a',
         array(
-          'href' => $view_root_uri.$view_tail_uri,
+          'href' => $drequest->generateURI(
+            array(
+              'path' => null,
+            ) + $uri_params),
         ),
         $view_name);
 
@@ -263,15 +270,16 @@ abstract class DiffusionController extends PhabricatorController {
         $path_sections[] = phutil_render_tag(
           'a',
           array(
-            'href' => $view_root_uri.$thus_far.$view_tail_uri,
+            'href' => $drequest->generateURI(
+              array(
+                'path' => $thus_far,
+              ) + $uri_params),
           ),
           phutil_escape_html($path_part));
       }
 
       $path_sections[] = phutil_escape_html($last);
       $path_sections = '/'.implode('/', $path_sections);
-
-      $jump_href = $view_root_uri.$thus_far.$last;
 
       $crumb_list[] = $path_sections;
     }
@@ -282,7 +290,10 @@ abstract class DiffusionController extends PhabricatorController {
       $jump_link = phutil_render_tag(
         'a',
         array(
-          'href' => $jump_href,
+          'href' => $drequest->generateURI(
+            array(
+              'commit' => null,
+            ) + $uri_params),
         ),
         'Jump to HEAD');
       $last_crumb .= " @ {$commit_link} ({$jump_link})";

src/applications/diffusion/controller/commit/DiffusionCommitController.php
@@ -20,6 +20,13 @@ final class DiffusionCommitController extends DiffusionController {
 
   const CHANGES_LIMIT = 100;
 
+  public function willProcessRequest(array $data) {
+    // This controller doesn't use blob/path stuff, just pass the dictionary
+    // in directly instead of using the AphrontRequest parsing mechanism.
+    $drequest = DiffusionRequest::newFromDictionary($data);
+    $this->diffusionRequest = $drequest;
+  }
+
   public function processRequest() {
     $drequest = $this->getDiffusionRequest();
     $request = $this->getRequest();
@@ -192,11 +199,11 @@ final class DiffusionCommitController extends DiffusionController {
           }
         }
 
-        $branch = $drequest->getBranchURIComponent(
-          $drequest->getBranch());
-        $filename = $changeset->getFilename();
-        $reference = "{$branch}{$filename};".$drequest->getCommit();
-        $references[$key] = $reference;
+        $references[$key] = $drequest->generateURI(
+          array(
+            'action' => 'rendering-ref',
+            'path'   => $changeset->getFilename(),
+          ));
       }
 
       // TOOD: Some parts of the views still rely on properties of the

src/applications/diffusion/controller/commit/__init__.php
@@ -19,6 +19,7 @@ phutil_require_module('phabricator', 'applications/diffusion/data/pathchange');
 phutil_require_module('phabricator', 'applications/diffusion/query/contains/base');
 phutil_require_module('phabricator', 'applications/diffusion/query/pathchange/base');
 phutil_require_module('phabricator', 'applications/diffusion/query/pathid/base');
+phutil_require_module('phabricator', 'applications/diffusion/request/base');
 phutil_require_module('phabricator', 'applications/diffusion/view/commentlist');
 phutil_require_module('phabricator', 'applications/diffusion/view/commitchangetable');
 phutil_require_module('phabricator', 'applications/draft/storage/draft');

src/applications/diffusion/controller/diff/DiffusionDiffController.php
@@ -19,15 +19,12 @@
 final class DiffusionDiffController extends DiffusionController {
 
   public function willProcessRequest(array $data) {
-    $request = $this->getRequest();
-    if ($request->getStr('ref')) {
-      $parts = explode(';', $request->getStr('ref'));
-      $data['path'] = idx($parts, 0);
-      $data['commit'] = idx($parts, 1);
-    }
+    $data = $data + array(
+      'dblob' => $this->getRequest()->getStr('ref'),
+    );
+    $drequest = DiffusionRequest::newFromAphrontRequestDictionary($data);
 
-    $this->diffusionRequest = DiffusionRequest::newFromAphrontRequestDictionary(
-      $data);
+    $this->diffusionRequest = $drequest;
   }
 
   public function processRequest() {

tomo/facebook-phabricator

commit 19b4243038e9c9814eb8c65e156b23ed42a1f346

Fix commit parsing in owners tool after bug introduced in 30ae22bfcff71fa3f14e38fd3cd597448df501c3
Summary:
A bug was introduced in
rP30ae22bfcff71fa3f14e38fd3cd597448df501c3 which caused commits to start
being parsed as if the repository callsign was empty. This caused errors
and problems and much unhappiness. Example error:

  EXCEPTION: (Exception) No such repository ''. at [.../phabricator/src/applications/diffusion/request/base/DiffusionRequest.php:130]

Test Plan: Saw that commit parsing was breaking. Applied fix. Saw that commits parsed again.

Reviewers: epriestley

CC: aran, epriestley

Differential Revision: https://secure.phabricator.com/D1974

src/applications/owners/query/path/PhabricatorOwnerPathQuery.php
@@ -22,7 +22,7 @@ final class PhabricatorOwnerPathQuery {
     PhabricatorRepository $repository,
     PhabricatorRepositoryCommit $commit) {
 
-    $drequest = DiffusionRequest::newFromAphrontRequestDictionary(
+    $drequest = DiffusionRequest::newFromDictionary(
       array(
         'repository'  => $repository,
         'commit'      => $commit->getCommitIdentifier(),