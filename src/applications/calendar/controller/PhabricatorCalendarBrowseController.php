tomo/facebook-phabricator

commit 0638c62294fcdf81fb1574c1fee57419bde23221

Create a status tool by giving /calendar/ some teeth
Summary: you can now add, edit, and delete status events. also added a "description" to status events and surface it in the big calendar view on mouse hover. some refactoring changes as well to make validation logic centralized within the storage class.

Test Plan: added, edited, deleted. yay.

Reviewers: epriestley, vrana

Reviewed By: epriestley

CC: aran, Korvin

Maniphest Tasks: T407

Differential Revision: https://secure.phabricator.com/D3810

src/applications/calendar/controller/PhabricatorCalendarBrowseController.php
@@ -20,12 +20,13 @@ final class PhabricatorCalendarBrowseController
   extends PhabricatorCalendarController {
 
   public function processRequest() {
+    $now     = time();
     $request = $this->getRequest();
-    $user = $request->getUser();
-
-    // TODO: These should be user-based and navigable in the interface.
-    $year = idate('Y');
-    $month = idate('m');
+    $user    = $request->getUser();
+    $year_d  = phabricator_format_local_time($now, $user, 'Y');
+    $year    = $request->getInt('year', $year_d);
+    $month_d = phabricator_format_local_time($now, $user, 'm');
+    $month   = $request->getInt('month', $month_d);
 
     $holidays = id(new PhabricatorCalendarHoliday())->loadAllWhere(
       'day BETWEEN %s AND %s',
@@ -39,6 +40,7 @@ final class PhabricatorCalendarBrowseController
         strtotime("{$year}-{$month}-01 next month"));
 
     $month_view = new AphrontCalendarMonthView($month, $year);
+    $month_view->setBrowseURI($request->getRequestURI());
     $month_view->setUser($user);
     $month_view->setHolidays($holidays);
 
@@ -53,18 +55,53 @@ final class PhabricatorCalendarBrowseController
       $status_text = $status->getTextStatus();
       $event->setUserPHID($status->getUserPHID());
       $event->setName("{$name_text} ({$status_text})");
-      $event->setDescription($status->getStatusDescription($user));
+      $details = '';
+      if ($status->getDescription()) {
+        $details = "\n\n".rtrim(phutil_escape_html($status->getDescription()));
+      }
+      $event->setDescription(
+        $status->getTerseSummary($user).$details
+      );
       $month_view->addEvent($event);
     }
 
-    return $this->buildStandardPageResponse(
+    $nav = $this->buildSideNavView();
+    $nav->selectFilter('edit');
+    $nav->appendChild(
       array(
+        $this->getNoticeView(),
         '<div style="padding: 2em;">',
           $month_view,
         '</div>',
-      ),
-      array(
+      ));
+
+    return $this->buildApplicationPage(
+     $nav,
+     array(
         'title' => 'Calendar',
+        'device' => true,
       ));
   }
+
+  private function getNoticeView() {
+    $request = $this->getRequest();
+    $view    = null;
+
+    if ($request->getExists('created')) {
+      $view = id(new AphrontErrorView())
+        ->setSeverity(AphrontErrorView::SEVERITY_NOTICE)
+        ->setTitle(pht('Successfully created your status.'));
+    } else if ($request->getExists('updated')) {
+      $view = id(new AphrontErrorView())
+        ->setSeverity(AphrontErrorView::SEVERITY_NOTICE)
+        ->setTitle(pht('Successfully updated your status.'));
+    } else if ($request->getExists('deleted')) {
+      $view = id(new AphrontErrorView())
+        ->setSeverity(AphrontErrorView::SEVERITY_NOTICE)
+        ->setTitle(pht('Successfully deleted your status.'));
+    }
+
+    return $view;
+  }
+
 }

src/applications/people/storage/PhabricatorUserStatus.php
@@ -18,24 +18,28 @@
 
 final class PhabricatorUserStatus extends PhabricatorUserDAO {
 
-  const STATUS_AWAY = 1;
-  const STATUS_SPORADIC = 2;
-
-  private static $statusTexts = array(
-    self::STATUS_AWAY => 'away',
-    self::STATUS_SPORADIC => 'sporadic',
-  );
-
   protected $userPHID;
   protected $dateFrom;
   protected $dateTo;
   protected $status;
+  protected $description;
 
-  public function getTextStatus() {
-    return self::$statusTexts[$this->status];
+  const STATUS_AWAY = 1;
+  const STATUS_SPORADIC = 2;
+
+  public function getStatusOptions() {
+    return array(
+      self::STATUS_AWAY     => pht('Away'),
+      self::STATUS_SPORADIC => pht('Sporadic'),
+    );
   }
 
-  public function getStatusDescription(PhabricatorUser $viewer) {
+  public function getTextStatus() {
+    $options = $this->getStatusOptions();
+    return $options[$this->status];
+  }
+
+  public function getTerseSummary(PhabricatorUser $viewer) {
     $until = phabricator_date($this->dateTo, $viewer);
     if ($this->status == PhabricatorUserStatus::STATUS_SPORADIC) {
       return 'Sporadic until '.$until;
@@ -45,7 +49,7 @@ final class PhabricatorUserStatus extends PhabricatorUserDAO {
   }
 
   public function setTextStatus($status) {
-    $statuses = array_flip(self::$statusTexts);
+    $statuses = array_flip($this->getStatusOptions());
     return $this->setStatus($statuses[$status]);
   }
 
@@ -54,6 +58,40 @@ final class PhabricatorUserStatus extends PhabricatorUserDAO {
       'userPHID IN (%Ls) AND UNIX_TIMESTAMP() BETWEEN dateFrom AND dateTo',
       $user_phids);
     return mpull($statuses, null, 'getUserPHID');
+  }
+
+  /**
+   * Validates data and throws exceptions for non-sensical status
+   * windows and attempts to create an overlapping status.
+   */
+  public function save() {
+
+    if ($this->getDateTo() <= $this->getDateFrom()) {
+      throw new PhabricatorUserStatusInvalidEpochException();
+    }
+
+    $this->openTransaction();
+    $this->beginWriteLocking();
+
+    if ($this->shouldInsertWhenSaved()) {
+
+      $overlap = $this->loadAllWhere(
+        'userPHID = %s AND dateFrom < %d AND dateTo > %d',
+        $this->getUserPHID(),
+        $this->getDateTo(),
+        $this->getDateFrom());
+
+      if ($overlap) {
+        $this->endWriteLocking();
+        $this->killTransaction();
+        throw new PhabricatorUserStatusOverlapException();
+      }
+    }
+
+    parent::save();
+
+    $this->endWriteLocking();
+    return $this->saveTransaction();
   }
 
 }

src/applications/conduit/method/user/ConduitAPI_user_addstatus_Method.php
@@ -31,9 +31,10 @@ final class ConduitAPI_user_addstatus_Method extends ConduitAPI_user_Method {
 
   public function defineParamTypes() {
     return array(
-      'fromEpoch' => 'required int',
-      'toEpoch' => 'required int',
-      'status' => 'required enum<away, sporadic>',
+      'fromEpoch'   => 'required int',
+      'toEpoch'     => 'required int',
+      'status'      => 'required enum<away, sporadic>',
+      'description' => 'optional string',
     );
   }
 
@@ -44,44 +45,31 @@ final class ConduitAPI_user_addstatus_Method extends ConduitAPI_user_Method {
   public function defineErrorTypes() {
     return array(
       'ERR-BAD-EPOCH' => "'toEpoch' must be bigger than 'fromEpoch'.",
-      'ERR-OVERLAP' =>
+      'ERR-OVERLAP'   =>
         'There must be no status in any part of the specified epoch.',
     );
   }
 
   protected function execute(ConduitAPIRequest $request) {
-    $user_phid = $request->getUser()->getPHID();
-    $from = $request->getValue('fromEpoch');
-    $to = $request->getValue('toEpoch');
+    $user_phid   = $request->getUser()->getPHID();
+    $from        = $request->getValue('fromEpoch');
+    $to          = $request->getValue('toEpoch');
+    $status      = ucfirst($request->getValue('status'));
+    $description = $request->getValue('description');
 
-    if ($to <= $from) {
+    try {
+      id(new PhabricatorUserStatus())
+        ->setUserPHID($user_phid)
+        ->setDateFrom($from)
+        ->setDateTo($to)
+        ->setTextStatus($status)
+        ->setDescription($description)
+        ->save();
+    } catch (PhabricatorUserStatusInvalidEpochException $e) {
       throw new ConduitException('ERR-BAD-EPOCH');
-    }
-
-    $table = new PhabricatorUserStatus();
-    $table->openTransaction();
-    $table->beginWriteLocking();
-
-    $overlap = $table->loadAllWhere(
-      'userPHID = %s AND dateFrom < %d AND dateTo > %d',
-      $user_phid,
-      $to,
-      $from);
-    if ($overlap) {
-      $table->endWriteLocking();
-      $table->killTransaction();
+    } catch (PhabricatorUserStatusOverlapException $e) {
       throw new ConduitException('ERR-OVERLAP');
     }
-
-    id(new PhabricatorUserStatus())
-      ->setUserPHID($user_phid)
-      ->setDateFrom($from)
-      ->setDateTo($to)
-      ->setTextStatus($request->getValue('status'))
-      ->save();
-
-    $table->endWriteLocking();
-    $table->saveTransaction();
   }
 
 }

resources/sql/patches/statustxt.sql
@@ -1,0 +1,2 @@
+ALTER TABLE {$NAMESPACE}_user.user_status
+  ADD description LONGTEXT NOT NULL COLLATE utf8_bin;

src/__phutil_library_map__.php
@@ -553,6 +553,7 @@ phutil_register_library_map(array(
     'PhabricatorApplicationApplications' => 'applications/meta/application/PhabricatorApplicationApplications.php',
     'PhabricatorApplicationAudit' => 'applications/audit/application/PhabricatorApplicationAudit.php',
     'PhabricatorApplicationAuth' => 'applications/auth/application/PhabricatorApplicationAuth.php',
+    'PhabricatorApplicationCalendar' => 'applications/calendar/application/PhabricatorApplicationCalendar.php',
     'PhabricatorApplicationConduit' => 'applications/conduit/application/PhabricatorApplicationConduit.php',
     'PhabricatorApplicationCountdown' => 'applications/countdown/application/PhabricatorApplicationCountdown.php',
     'PhabricatorApplicationDaemons' => 'applications/daemon/application/PhabricatorApplicationDaemons.php',
@@ -609,8 +610,11 @@ phutil_register_library_map(array(
     'PhabricatorCalendarBrowseController' => 'applications/calendar/controller/PhabricatorCalendarBrowseController.php',
     'PhabricatorCalendarController' => 'applications/calendar/controller/PhabricatorCalendarController.php',
     'PhabricatorCalendarDAO' => 'applications/calendar/storage/PhabricatorCalendarDAO.php',
+    'PhabricatorCalendarDeleteStatusController' => 'applications/calendar/controller/PhabricatorCalendarDeleteStatusController.php',
+    'PhabricatorCalendarEditStatusController' => 'applications/calendar/controller/PhabricatorCalendarEditStatusController.php',
     'PhabricatorCalendarHoliday' => 'applications/calendar/storage/PhabricatorCalendarHoliday.php',
     'PhabricatorCalendarHolidayTestCase' => 'applications/calendar/storage/__tests__/PhabricatorCalendarHolidayTestCase.php',
+    'PhabricatorCalendarViewStatusController' => 'applications/calendar/controller/PhabricatorCalendarViewStatusController.php',
     'PhabricatorChangesetResponse' => 'infrastructure/diff/PhabricatorChangesetResponse.php',
     'PhabricatorChatLogChannelListController' => 'applications/chatlog/controller/PhabricatorChatLogChannelListController.php',
     'PhabricatorChatLogChannelLogController' => 'applications/chatlog/controller/PhabricatorChatLogChannelLogController.php',
@@ -1125,6 +1129,8 @@ phutil_register_library_map(array(
     'PhabricatorUserProfile' => 'applications/people/storage/PhabricatorUserProfile.php',
     'PhabricatorUserSSHKey' => 'applications/settings/storage/PhabricatorUserSSHKey.php',
     'PhabricatorUserStatus' => 'applications/people/storage/PhabricatorUserStatus.php',
+    'PhabricatorUserStatusInvalidEpochException' => 'applications/people/exception/PhabricatorUserStatusInvalidEpochException.php',
+    'PhabricatorUserStatusOverlapException' => 'applications/people/exception/PhabricatorUserStatusOverlapException.php',
     'PhabricatorUserTestCase' => 'applications/people/storage/__tests__/PhabricatorUserTestCase.php',
     'PhabricatorWorker' => 'infrastructure/daemon/workers/PhabricatorWorker.php',
     'PhabricatorWorkerDAO' => 'infrastructure/daemon/workers/storage/PhabricatorWorkerDAO.php',
@@ -1748,6 +1754,7 @@ phutil_register_library_map(array(
     'PhabricatorApplicationApplications' => 'PhabricatorApplication',
     'PhabricatorApplicationAudit' => 'PhabricatorApplication',
     'PhabricatorApplicationAuth' => 'PhabricatorApplication',
+    'PhabricatorApplicationCalendar' => 'PhabricatorApplication',
     'PhabricatorApplicationConduit' => 'PhabricatorApplication',
     'PhabricatorApplicationCountdown' => 'PhabricatorApplication',
     'PhabricatorApplicationDaemons' => 'PhabricatorApplication',
@@ -1780,7 +1787,11 @@ phutil_register_library_map(array(
     'PhabricatorApplicationUIExamples' => 'PhabricatorApplication',
     'PhabricatorApplicationsListController' => 'PhabricatorController',
     'PhabricatorAuditAddCommentController' => 'PhabricatorAuditController',
-    'PhabricatorAuditComment' => 'PhabricatorAuditDAO',
+    'PhabricatorAuditComment' =>
+    array(
+      0 => 'PhabricatorAuditDAO',
+      1 => 'PhabricatorMarkupInterface',
+    ),
     'PhabricatorAuditCommentEditor' => 'PhabricatorEditor',
     'PhabricatorAuditCommitListView' => 'AphrontView',
     'PhabricatorAuditController' => 'PhabricatorController',
@@ -1803,8 +1814,11 @@ phutil_register_library_map(array(
     'PhabricatorCalendarBrowseController' => 'PhabricatorCalendarController',
     'PhabricatorCalendarController' => 'PhabricatorController',
     'PhabricatorCalendarDAO' => 'PhabricatorLiskDAO',
+    'PhabricatorCalendarDeleteStatusController' => 'PhabricatorCalendarController',
+    'PhabricatorCalendarEditStatusController' => 'PhabricatorCalendarController',
     'PhabricatorCalendarHoliday' => 'PhabricatorCalendarDAO',
     'PhabricatorCalendarHolidayTestCase' => 'PhabricatorTestCase',
+    'PhabricatorCalendarViewStatusController' => 'PhabricatorCalendarController',
     'PhabricatorChangesetResponse' => 'AphrontProxyResponse',
     'PhabricatorChatLogChannelListController' => 'PhabricatorChatLogController',
     'PhabricatorChatLogChannelLogController' => 'PhabricatorChatLogController',
@@ -2265,6 +2279,8 @@ phutil_register_library_map(array(
     'PhabricatorUserProfile' => 'PhabricatorUserDAO',
     'PhabricatorUserSSHKey' => 'PhabricatorUserDAO',
     'PhabricatorUserStatus' => 'PhabricatorUserDAO',
+    'PhabricatorUserStatusInvalidEpochException' => 'Exception',
+    'PhabricatorUserStatusOverlapException' => 'Exception',
     'PhabricatorUserTestCase' => 'PhabricatorTestCase',
     'PhabricatorWorkerDAO' => 'PhabricatorLiskDAO',
     'PhabricatorWorkerTask' => 'PhabricatorWorkerDAO',

src/aphront/configuration/AphrontDefaultApplicationConfiguration.php
@@ -113,10 +113,6 @@ class AphrontDefaultApplicationConfiguration
         'keyboardshortcut/' => 'PhabricatorHelpKeyboardShortcutController',
       ),
 
-       '/calendar/' => array(
-        '' => 'PhabricatorCalendarBrowseController',
-      ),
-
       '/drydock/' => array(
         '' => 'DrydockResourceListController',
         'resource/' => 'DrydockResourceListController',

src/applications/calendar/application/PhabricatorApplicationCalendar.php
@@ -1,0 +1,58 @@
+<?php
+
+/*
+ * Copyright 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+final class PhabricatorApplicationCalendar extends PhabricatorApplication {
+
+  public function getShortDescription() {
+    return pht('Dates and Stuff');
+  }
+
+  public function getFlavorText() {
+    return pht('Never miss an episode ever again.');
+  }
+
+  public function getBaseURI() {
+    return '/calendar/';
+  }
+
+  public function getTitleGlyph() {
+    // Unicode has a calendar character but it's in some distant code plane,
+    // use "keyboard" since it looks vaguely similar.
+    return "\xE2\x8C\xA8";
+  }
+
+  public function getRoutes() {
+    return array(
+      '/calendar/' => array(
+        '' => 'PhabricatorCalendarBrowseController',
+        'status/' => array(
+          '' => 'PhabricatorCalendarViewStatusController',
+          'create/' =>
+            'PhabricatorCalendarEditStatusController',
+          'delete/(?P<id>[1-9]\d*)/' =>
+            'PhabricatorCalendarDeleteStatusController',
+          'edit/(?P<id>[1-9]\d*)/' =>
+            'PhabricatorCalendarEditStatusController',
+          'view/(?P<phid>[^/]+)/' =>
+            'PhabricatorCalendarViewStatusController',
+        ),
+      ),
+    );
+  }
+
+}

src/applications/calendar/controller/PhabricatorCalendarController.php
@@ -18,21 +18,26 @@
 
 abstract class PhabricatorCalendarController extends PhabricatorController {
 
-  public function buildStandardPageResponse($view, array $data) {
 
-    $page = $this->buildStandardPageView();
+  protected function buildSideNavView(PhabricatorUserStatus $status = null) {
+    $nav = new AphrontSideNavFilterView();
+    $nav->setBaseURI(new PhutilURI($this->getApplicationURI()));
 
-    $page->setApplicationName('Calendar');
-    $page->setBaseURI('/calendar/');
-    $page->setTitle(idx($data, 'title'));
+    $nav->addFilter('', pht('Calendar'), $this->getApplicationURI());
 
-    // Unicode has a calendar character but it's in some distant code plane,
-    // use "keyboard" since it looks vaguely similar.
-    $page->setGlyph("\xE2\x8C\xA8");
-    $page->appendChild($view);
+    $nav->addSpacer();
 
-    $response = new AphrontWebpageResponse();
-    return $response->setContent($page->render());
+    $nav->addLabel(pht('Create Events'));
+    $nav->addFilter('status/create/', pht('New Status'));
+
+    $nav->addSpacer();
+    $nav->addLabel(pht('Your Events'));
+    if ($status && $status->getID()) {
+      $nav->addFilter('status/edit/'.$status->getID().'/', pht('Edit Status'));
+    }
+    $nav->addFilter('status/', pht('Upcoming Statuses'));
+
+    return $nav;
   }
 
 }

commit 0792305eb07551f077882cb10fa2e30e3453c1d2

Fix displaying of user status
Summary:
This fixes two separate issues:

# `getTextStatus()` is used for machine readable data in handles and user.info method. Broken since D3810.
# Status may contain date. Broken since beginning but masked by the fact that CSS ignores unknown class names.

Test Plan:
Displayed revision with reviewer away.
Called `user.addstatus`.
Edited status in calendar.

Reviewers: btrahan, epriestley

Reviewed By: epriestley

CC: nh, aran, Korvin

Differential Revision: https://secure.phabricator.com/D4275

src/applications/calendar/controller/PhabricatorCalendarBrowseController.php
@@ -36,7 +36,7 @@ final class PhabricatorCalendarBrowseController
       $event->setEpochRange($status->getDateFrom(), $status->getDateTo());
 
       $name_text = $handles[$status->getUserPHID()]->getName();
-      $status_text = $status->getTextStatus();
+      $status_text = $status->getHumanStatus();
       $event->setUserPHID($status->getUserPHID());
       $event->setName("{$name_text} ({$status_text})");
       $details = '';

src/applications/people/conduit/ConduitAPI_user_addstatus_Method.php
@@ -38,7 +38,7 @@ final class ConduitAPI_user_addstatus_Method extends ConduitAPI_user_Method {
     $user_phid   = $request->getUser()->getPHID();
     $from        = $request->getValue('fromEpoch');
     $to          = $request->getValue('toEpoch');
-    $status      = ucfirst($request->getValue('status'));
+    $status      = $request->getValue('status');
     $description = $request->getValue('description', '');
 
     try {

src/applications/people/storage/PhabricatorUserStatus.php
@@ -11,6 +11,15 @@ final class PhabricatorUserStatus extends PhabricatorUserDAO {
   const STATUS_AWAY = 1;
   const STATUS_SPORADIC = 2;
 
+  private static $statusTexts = array(
+    self::STATUS_AWAY => 'away',
+    self::STATUS_SPORADIC => 'sporadic',
+  );
+
+  public function getTextStatus() {
+    return self::$statusTexts[$this->status];
+  }
+
   public function getStatusOptions() {
     return array(
       self::STATUS_AWAY     => pht('Away'),
@@ -18,7 +27,7 @@ final class PhabricatorUserStatus extends PhabricatorUserDAO {
     );
   }
 
-  public function getTextStatus() {
+  public function getHumanStatus() {
     $options = $this->getStatusOptions();
     return $options[$this->status];
   }
@@ -33,7 +42,7 @@ final class PhabricatorUserStatus extends PhabricatorUserDAO {
   }
 
   public function setTextStatus($status) {
-    $statuses = array_flip($this->getStatusOptions());
+    $statuses = array_flip(self::$statusTexts);
     return $this->setStatus($statuses[$status]);
   }
 

src/applications/phid/PhabricatorObjectHandle.php
@@ -12,6 +12,7 @@ final class PhabricatorObjectHandle {
   private $timestamp;
   private $alternateID;
   private $status = PhabricatorObjectHandleStatus::STATUS_OPEN;
+  private $title;
   private $complete;
   private $disabled;
 
@@ -49,6 +50,11 @@ final class PhabricatorObjectHandle {
 
   public function getStatus() {
     return $this->status;
+  }
+
+  public function setTitle($title) {
+    $this->title = $title;
+    return $this;
   }
 
   public function setFullName($full_name) {
@@ -176,7 +182,7 @@ final class PhabricatorObjectHandle {
 
     if ($this->status != PhabricatorObjectHandleStatus::STATUS_OPEN) {
       $class .= ' handle-status-'.$this->status;
-      $title = $this->status;
+      $title = (isset($this->title) ? $this->title : $this->status);
     }
 
     if ($this->disabled) {

src/applications/phid/handle/PhabricatorObjectHandleData.php
@@ -223,12 +223,11 @@ final class PhabricatorObjectHandleData {
               $handle->setAlternateID($user->getID());
               $handle->setComplete(true);
               if (isset($statuses[$phid])) {
-                $status = $statuses[$phid]->getTextStatus();
+                $handle->setStatus($statuses[$phid]->getTextStatus());
                 if ($this->viewer) {
-                  $date = $statuses[$phid]->getDateTo();
-                  $status .= ' until '.phabricator_date($date, $this->viewer);
+                  $handle->setTitle(
+                    $statuses[$phid]->getTerseSummary($this->viewer));
                 }
-                $handle->setStatus($status);
               }
               $handle->setDisabled($user->getIsDisabled());
 